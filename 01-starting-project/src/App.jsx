a599599c583be7467a9c3bc23e07a3c3211aab86
KimMInJi22 <manji9863@gmail.com>
2025-03-14 21:32:39 +0900

    feat: 20250314 useState / 삼항연산자 / 객체조회방식 두가지

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -3,6 +3,7 @@ import { CORE_CONCEPTS } from "./data.js";
 import CoreConcepts from "./components/CoreConcepts.jsx";
 import TabButton from "./components/TabButton.jsx";
 import { useState } from "react";
+import { EXAMPLES } from "./data_updated.js";
 
 function App() {
 
@@ -11,13 +12,14 @@ function App() {
   function handleSelect(selectedTopic) {
     // selectedButton = 'components', 'jsx', 'props', 'state'
     console.log(selectedTopic);
+    setSelectedTopic(selectedTopic);
   }
 
   return (
     <div>
       <Header2 />
       <main>
-      <section id="core-concepts">
+      <section id="core-concepts"> 
             <h2>핵심 개념들 of React</h2>
             <ul>
                 <CoreConcepts image={CORE_CONCEPTS[0].image} title={CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} />
@@ -29,12 +31,22 @@ function App() {
         <section id="examples"> 
           <h2>예시들</h2>
             <menu>
-              <TabButton onSelect={handleSelect('굿나잇')}>Components</TabButton>
-              <TabButton>JSX</TabButton>
-              <TabButton>Props</TabButton>
-              <TabButton>State</TabButton>
+              <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+              <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+              <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+              <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
             </menu>
-            동적인 버튼 클릭의 결과물
+            {selectedTopic === '' ? <h3>버튼을 클릭해보세요.</h3> : 
+              <div id="tab-content">
+              <h3>{EXAMPLES[selectedTopic].title}</h3>
+                <p>{EXAMPLES[selectedTopic].description}</p>
+                <pre>
+                  <code>
+                    {EXAMPLES[selectedTopic].code}
+                  </code>
+                </pre>  
+              </div>
+            }
         </section>
       </main>
     </div>
