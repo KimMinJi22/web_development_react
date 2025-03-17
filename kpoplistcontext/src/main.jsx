d82d1c6711993003c4bbd3f44d8e031affec870b
KimMInJi22 <manji9863@gmail.com>
2025-03-17 21:45:44 +0900

    fead: 20250317 context API lesson

--- kpoplistcontext/src/main.jsx
+++ kpoplistcontext/src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { KpopContextProvider } from './context/context.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <KpopContextProvider>
+      <App />
+    </KpopContextProvider>
   </React.StrictMode>,
 )
