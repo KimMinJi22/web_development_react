be2482843915f8b95ade3654c3078cb2fea650ba
KimMInJi22 <manji9863@gmail.com>
2025-03-21 21:33:47 +0900

    feat: 20250321 spread syntax / .map() method / todolist

--- reacttodoapp/src/App.jsx
+++ reacttodoapp/src/App.jsx
@@ -1,9 +1,42 @@
+import { useState } from "react";
+import './App.css';
+
 function App() {
+  const [ toDo, setToDo ] = useState(''); // 얘네가 element가 됨
+  const [ toDos, setToDos ] = useState([]); // 얘가 배열인데 ↑
+
+  const onChange = (event) => {
+    setToDo(event.target.value)
+  };
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    if(toDo === '') {
+      return;
+    }
+    
+    //        ↓배열                   ↓element들의 자료형 - string
+    setToDos((currentArray) => [toDo, ...currentArray]); // spread 문법
+    setToDo(''); // 이상의 조건문이 실행되지 않았다면 버튼 클릭 후에 input창을 비움
+  };
 
   return (
-    <>
-      
-    </>
+    <div>
+      <h1>My To Dos</h1>
+      <form onSubmit={onSubmit}>
+        <input 
+          type="text" 
+          onChange={onChange}
+          value={toDo}
+          placeholder="Type Your To Do 📝" />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
+    </div>
   )
 }
 
